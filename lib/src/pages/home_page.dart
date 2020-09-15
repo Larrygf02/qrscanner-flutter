6f21d67c20a3686012495ec506460ef02291400f
Larrygf02 <raulgf_02@hotmail.com>
2020-09-14 21:33:30 -0500

    BottomNavigator

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+int currentIndex = 0;
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Home page'),
-      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
     );
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex, // indica que elemento esta activo
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        )
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch( paginaActual ) {
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+      default: return MapasPage();
+    }
   }
 }
